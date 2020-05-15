fdc819d2bd681cb4fb41f6817f2765ec307dd8ec
shahzad.bhatti1 <shahzad.bhatti1@digital.hmrc.gov.uk>
2020-05-15 13:20:41 +0100

    DIT-1997: trader perf tests

--- README.md
+++ README.md
@@ -1,8 +1,34 @@
+# digital-tariffs-trader-performance-tests
 
+This is a placeholder README.md for a new repository
+
+## Commands for running the Gatling tests from the local terminal
+
+#### command for running all Gatling simulations 
+```
+sbt gatling:test or 
+run the shell script for all tests by executing: ./run.sh
+
+To run smoke test run shell script for smoke tests by executing: ./run_smoke_test.sh or 
+sbt -Dperftest.runSmokeTest=true -DrunLocal=true gatling:test
+
+```
+
+## Jenkins
+
+#### Jenkins job
+https://performance.tools.staging.tax.service.gov.uk/job/digital-tariffs-trader-performance-tests/
+
+#### Jenkins configurations
+https://github.com/hmrc/performance-test-jobs/blob/master/jobs/live/digital_tariffs.groovy
+---
+
+#### Performance testing documentation 
+https://confluence.tools.tax.service.gov.uk/display/DTRG/Performance+Testing
+=======
 # digital-tariffs-trader-performance-tests
 
 This is a placeholder README.md for a new repository
 
 ### License
-
 This code is open source software licensed under the [Apache 2.0 License]("http://www.apache.org/licenses/LICENSE-2.0.html").
--- src/test/scala/uk/gov/hmrc/perftests/digitaltariffs/traderui/TraderUiSimulation.scala
+++ src/test/scala/uk/gov/hmrc/perftests/digitaltariffs/traderui/TraderUiSimulation.scala
@@ -1,6 +1,5 @@
 package uk.gov.hmrc.perftests.digitaltariffs.traderui
 
-import io.gatling.core.Predef._
 import io.gatling.http.protocol.HttpProtocolBuilder
 import uk.gov.hmrc.performance.simulation.PerformanceTestRunner
 import uk.gov.hmrc.perftests.digitaltariffs.DigitalTariffsPerformanceTestRunner
@@ -13,40 +12,38 @@ class TraderUiSimulation extends PerformanceTestRunner with DigitalTariffsPerfor
     buildHttpProtocol(url = externalBaseUrl)
   }
 
-  private val scn =
-    scenario("UK Trader applies for a BTI application")
-      // Government Gateway Sign In
-      .exec(getGovGatewaySignIn).exec(pause(waitTime))
-      .exec(postGovGatewaySignIn).exec(pause(waitTime))
-      // Trader UI journey
-      .exec(getStartPage).exec(pause(waitTime))
-      .exec(getImportOrExport).exec(pause(waitTime))
-      .exec(postImportOrExport).exec(pause(waitTime))
-      .exec(getInformationYouNeed).exec(pause(waitTime))
-      .exec(getRegisterForEori).exec(pause(waitTime))
-      .exec(postRegisterForEori).exec(pause(waitTime))
-      .exec(postEnterContactDetails).exec(pause(waitTime))
-      .exec(postWhichBestDescribesYou).exec(pause(waitTime))
-      .exec(postSelectApplicationType).exec(pause(waitTime))
-      .exec(getAcceptItemInfoList).exec(pause(waitTime))
-      .exec(postDescribeYourItem).exec(pause(waitTime))
-      .exec(postSupportingMaterialFileList).exec(pause(waitTime))
-      .exec(postCommodityCodeBestMatch).exec(pause(waitTime))
-      .exec(postCommodityCodeDigits).exec(pause(waitTime))
-      .exec(postWhenToSendSample).exec(pause(waitTime))
-      .exec(postReturnSample).exec(pause(waitTime))
-      .exec(postSimilarItemCommodityCode).exec(pause(waitTime))
-      .exec(postLegalChallenge).exec(pause(waitTime))
-      .exec(postSupportingInformation).exec(pause(waitTime))
-      .exec(postSupportingInformationDetails).exec(pause(waitTime))
-      .exec(getCheckYourAnswers).exec(pause(waitTime))
-      .exec(getDeclaration).exec(pause(waitTime))
-      .exec(postDeclaration).exec(pause(waitTime))
-      .exec(getConfirmation).exec(pause(waitTime))
+  setup("traderUI", "UK Trader applies for a BTI application") withRequests(
+    // Government Gateway Sign In
+    getGovGatewaySignIn,
+    postGovGatewaySignIn,
+    // Trader UI journey
+    getStartPage,
+    getImportOrExport,
+    postImportOrExport,
+    getInformationYouNeed,
+    getRegisterForEori,
+    postRegisterForEori,
+    postEnterContactDetails,
+    postWhichBestDescribesYou,
+    postSelectApplicationType,
+    getAcceptItemInfoList,
+    postDescribeYourItem,
+    postSupportingMaterialFileList,
+    postCommodityCodeBestMatch,
+    postCommodityCodeDigits,
+    postWhenToSendSample,
+    postReturnSample,
+    postSimilarItemCommodityCode,
+    postLegalChallenge,
+    postSupportingInformation,
+    postSupportingInformationDetails,
+    getCheckYourAnswers,
+    getDeclaration,
+    postDeclaration,
+    getConfirmation
 
-  // TODO: when the `performance-test-runner` is fixed, we should use it here
-  setUp(scn.inject(simulationSteps))
-    .protocols(httpProtocol)
-    .assertions(simulationAssertion)
+  )
+
+  runSimulation()
 
 }
