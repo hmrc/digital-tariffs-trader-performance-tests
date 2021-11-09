6c0d4a0486f51f92357d7a2c8b6711414e4d5e1f
Kuldeep Wadhwa <11062122+kuldeepwadhwa9@users.noreply.github.com>
2021-11-09 17:35:44 +0000

    DDCE-2354 Fixed trader tests (#10)

---
+++ run_local.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sbt gatling:test -DrunLocal=true
--- src/test/scala/uk/gov/hmrc/perftests/digitaltariffs/traderui/AuthRequests.scala
+++ src/test/scala/uk/gov/hmrc/perftests/digitaltariffs/traderui/AuthRequests.scala
@@ -10,14 +10,12 @@ object AuthRequests extends DigitalTariffsPerformanceTestRunner {
   def getGovGatewaySignIn: HttpRequestBuilder = {
     http("Government Gateway Sign In - GET")
       .get(s"$authStubBaseUrl/gg-sign-in")
-      .check(css("input[name='csrfToken']", "value").saveAs("csrfToken"))
       .check(status.is(200))
   }
 
   def postGovGatewaySignIn: HttpRequestBuilder = {
     http("Government Gateway Sign In - POST")
       .post(s"$authStubBaseUrl/gg-sign-in")
-      .formParam("csrfToken", "you-do-not-need-this-anymore")
       .formParam("authorityId", "")
       .formParam("redirectionUrl", traderUiBaseUrl)
       .formParam("credentialStrength", "weak")
