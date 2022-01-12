0e71d2012570ebfba4d46e0660d242d069ef460e
Kannan Ekambaram <83719946+kannanekambaram@users.noreply.github.com>
2022-01-12 11:29:18 +0000

    DDCE-2549 : ATAR Application Service: Create new page and update content about communicating by email (#12)

--- src/test/scala/uk/gov/hmrc/perftests/digitaltariffs/traderui/TraderUiSimulation.scala
+++ src/test/scala/uk/gov/hmrc/perftests/digitaltariffs/traderui/TraderUiSimulation.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 HM Revenue & Customs
+ * Copyright 2022 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -36,6 +36,7 @@ class TraderUiSimulation extends PerformanceTestRunner with DigitalTariffsPerfor
     getStartPage,
     getInformationYouNeed,
     getInformationMadePublic,
+    getHowWeContactYou,
     getGoodsName,
     postGoodsName,
     postGoodsDescription,
